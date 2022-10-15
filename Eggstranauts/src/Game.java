58aec6f1a8ebc1ea75fbc73b3d1dff72f0bac878
Alan0893 <alanl07905@gmail.com>
2022-10-15 12:42:41 -0400

    t

--- Eggstranauts/src/Game.java
+++ Eggstranauts/src/Game.java
@@ -49,7 +49,7 @@ public class Game extends JPanel{
                 player.jumping(-7);
          //   }
         }
-//
+
         if(keys[KeyEvent.VK_D]) {
             if (player.getGround()) {
                 player.move(6,0);
